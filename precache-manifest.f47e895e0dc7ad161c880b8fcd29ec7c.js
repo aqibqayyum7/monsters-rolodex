bdfc4f06fbe5d8c408303a2b1870494b9300eecf
Aqib Qayyum <aqib.qayyum@virtual-force.com>
2020-01-29 11:41:04 +0500

    Updates

---
+++ precache-manifest.f47e895e0dc7ad161c880b8fcd29ec7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01213741370498c3343eb05b48846c64",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1133cc09348b96c01f31",
+    "url": "/monsters-rolodex/static/css/main.d427c2cc.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1133cc09348b96c01f31",
+    "url": "/monsters-rolodex/static/js/main.16e79388.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
